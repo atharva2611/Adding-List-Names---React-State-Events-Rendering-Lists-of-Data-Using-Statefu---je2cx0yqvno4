14893c79c732f4d169aafbbaee910e61cdc0855a
atharva2611 <83395780+atharva2611@users.noreply.github.com>
2023-03-03 20:49:00 +0530

    updated-adding-list-names

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,15 @@
+
 import React from 'react';
 import ListItems from './ListItems';
 //code here
  const List = ({listx}) =>{
   return(
     
- 
-    <ListItems valuex={i} />
+    <>
+      <ul id='list'>
+        {listx.map((list  => <ListItems valuex ={list}/>))}
+      </ul>
+    </>
     
   
   )
